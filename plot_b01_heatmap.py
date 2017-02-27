41fe442f8130e21ba63074d447f4f57e4a55bd95
Andrew Tedstone <a.j.tedstone@bristol.ac.uk>
2017-02-27 10:43:36 +0000

    convert scripts to only create figures if run as __main__, if imported they only do data analysis/preparation

--- plot_b01_heatmap.py
+++ plot_b01_heatmap.py
@@ -1,5 +1,4 @@
 from prep_env_vars import *
-import seaborn as sns
 
 mod10 = xr.open_mfdataset('/scratch/MOD10A1.006.SW/*.nc', chunks={'TIME':366})
 
@@ -19,32 +18,37 @@ b01_avg = b01.where(al <= 100).where(mask_dark > 0).mean(dim=('X', 'Y'))
 #b01_avg = b01_avg.where(daily_perc_cloudy <= 50).where(daily_perc_ice > 90)
 b01_avg = b01_avg.where(daily_perc_cloudy <= 50)
 
-b01_pd = b01_avg.to_pandas().to_frame()
-b01_pd['Year'] = b01_pd.index.year
-b01_pd['DOY'] = b01_pd.index.dayofyear
-b01_piv = pd.pivot_table(b01_pd, index='DOY', columns='Year', values=0)
 
-rcParams['font.size'] = 6
-rcParams['axes.labelsize'] = 6 # sets colorbar label size
-rcParams['legend.fontsize'] = 6
-rcParams['xtick.labelsize'] = 6
-rcParams['ytick.labelsize'] = 6
-rcParams['xtick.direction'] = 'in'
-rcParams['xtick.major.pad'] = 3
-rcParams['figure.titlesize'] = 6
+if __name__ == '__main__':
 
-fig = plt.figure(figsize=(5.5, 2.5))
-ax = plt.subplot(111)
-plt.subplots_adjust(right=0.88, bottom=0.15)
-cb_ax = fig.add_axes((0.9, 0.15, 0.02, 0.75))
-h = sns.heatmap(b01_piv.T, ax=ax, cmap='YlOrRd_r', vmin=0.1, vmax=0.9, cbar_ax=cb_ax, cbar_kws=dict(label='B01 reflectance', ticks=(0.1, 0.3, 0.5, 0.7, 0.9)))
-plt.yticks(rotation=0)
-plt.xlabel('Date', fontsize=6)
-plt.ylabel('Year', fontsize=6)
-plt.xlim(0, 273-121)
-plt.xticks(np.array([121, 152, 182, 213, 244])-121, ['1 May', '1 Jun', '1 Jul', '1 Aug', '1 Sep'])
+	import seaborn as sns
 
-ax.xaxis.set_tick_params(length=3, pad=1, width=0.5)
-ax.spines['bottom'].axis.axes.tick_params(direction='outward')
+	b01_pd = b01_avg.to_pandas().to_frame()
+	b01_pd['Year'] = b01_pd.index.year
+	b01_pd['DOY'] = b01_pd.index.dayofyear
+	b01_piv = pd.pivot_table(b01_pd, index='DOY', columns='Year', values=0)
 
-plt.savefig('/home/at15963/Dropbox/work/black_and_bloom/papers/tedstone_darkice/submission1/figures/B01.pdf')
+	rcParams['font.size'] = 6
+	rcParams['axes.labelsize'] = 6 # sets colorbar label size
+	rcParams['legend.fontsize'] = 6
+	rcParams['xtick.labelsize'] = 6
+	rcParams['ytick.labelsize'] = 6
+	rcParams['xtick.direction'] = 'in'
+	rcParams['xtick.major.pad'] = 3
+	rcParams['figure.titlesize'] = 6
+
+	fig = plt.figure(figsize=(5.5, 2.5))
+	ax = plt.subplot(111)
+	plt.subplots_adjust(right=0.88, bottom=0.15)
+	cb_ax = fig.add_axes((0.9, 0.15, 0.02, 0.75))
+	h = sns.heatmap(b01_piv.T, ax=ax, cmap='YlOrRd_r', vmin=0.1, vmax=0.9, cbar_ax=cb_ax, cbar_kws=dict(label='B01 reflectance', ticks=(0.1, 0.3, 0.5, 0.7, 0.9)))
+	plt.yticks(rotation=0)
+	plt.xlabel('Date', fontsize=6)
+	plt.ylabel('Year', fontsize=6)
+	plt.xlim(0, 273-121)
+	plt.xticks(np.array([121, 152, 182, 213, 244])-121, ['1 May', '1 Jun', '1 Jul', '1 Aug', '1 Sep'])
+
+	ax.xaxis.set_tick_params(length=3, pad=1, width=0.5)
+	ax.spines['bottom'].axis.axes.tick_params(direction='outward')
+
+	plt.savefig('/home/at15963/Dropbox/work/black_and_bloom/papers/tedstone_darkice/submission1/figures/B01.pdf')
--- plot_energy_fluxes.py
+++ plot_energy_fluxes.py
@@ -2,17 +2,6 @@ from prep_env_vars import *
 
 mar_path_anomalies = '/scratch/MARv3.6.2-7.5km-v2-ERA/ICE.*nc'
 
-plt.style.use('default')
-rcParams['font.size'] = 6
-rcParams['font.sans-serif'] = 'Arial'
-rcParams['axes.labelsize'] = 6 # sets colorbar label size
-rcParams['legend.fontsize'] = 6
-rcParams['xtick.labelsize'] = 6
-rcParams['ytick.labelsize'] = 6
-#rcParams['xtick.direction'] = 'in'
-rcParams['xtick.major.pad'] = 3
-rcParams['figure.titlesize'] = 6
-
 # Radiative fluxes -----------------------------------------------------------
 SW_all_long = mar_raster.open_mfxr(mar_path_anomalies,
 	dim='TIME', transform_func=lambda ds: ds.SWD.sel(X=x_slice, 
@@ -80,85 +69,98 @@ RF_avg = RF_all.sel(TIME=slice('2000', '2016')) \
 
 # ============================================================================
 
-fig = plt.figure(figsize=(3.5, 4))
+if __name__ == '__main__':
 
-## Subplot: radiative fluxes -------------------------------------------------
-ax_rad = plt.subplot(3, 1, 1)
+	plt.style.use('default')
+	rcParams['font.size'] = 6
+	rcParams['font.sans-serif'] = 'Arial'
+	rcParams['axes.labelsize'] = 6 # sets colorbar label size
+	rcParams['legend.fontsize'] = 6
+	rcParams['xtick.labelsize'] = 6
+	rcParams['ytick.labelsize'] = 6
+	#rcParams['xtick.direction'] = 'in'
+	rcParams['xtick.major.pad'] = 3
+	rcParams['figure.titlesize'] = 6
 
-plt.axhline(y=0, color='gray', linewidth=0.5)
+	fig = plt.figure(figsize=(3.5, 4))
 
-LWD_JJA_anom.plot(label='LWd', marker='o', color='#E31A1C', markersize=4, mec='none')
-SWD_JJA_anom.plot(label='SWd', marker='o', color='#377EB8', markersize=4, mec='none')
-#plt.legend(numpoints=1)
-plt.ylim(-25, 25)
-plt.ylabel('W m$^{-2}$')
+	## Subplot: radiative fluxes -------------------------------------------------
+	ax_rad = plt.subplot(3, 1, 1)
 
+	plt.axhline(y=0, color='gray', linewidth=0.5)
 
-
-ax_rad.spines['left'].axis.axes.tick_params(direction='outward')
-ax_rad.yaxis.tick_left()
-#ax_rad.xaxis.tick_bottom()
-plt.tick_params(axis='x', bottom='off', top='off')
-ax_rad.spines['top'].set_visible(False)
-ax_rad.spines['right'].set_visible(False)
-ax_rad.spines['bottom'].set_visible(False)
-ax_rad.annotate('a', fontsize=8, fontweight='bold', xy=(0.05,0.95), xycoords='axes fraction',
-           horizontalalignment='left', verticalalignment='top')
+	LWD_JJA_anom.plot(label='LWd', marker='o', color='#E31A1C', markersize=4, mec='none')
+	SWD_JJA_anom.plot(label='SWd', marker='o', color='#377EB8', markersize=4, mec='none')
+	#plt.legend(numpoints=1)
+	plt.ylim(-25, 25)
+	plt.ylabel('W m$^{-2}$')
 
 
 
-## Subplot: other fluxes -----------------------------------------------------
-ax_precip = plt.subplot(3, 1, 2, sharex=ax_rad)
-precip_pd = RF_avg.to_pandas().to_frame()
-precip_pd = precip_pd.assign(width=(365 - 100))
-# Deal with leap years
-precip_pd.loc[precip_pd.index.is_leap_year, 'width'] = 366 - 100
-plt.bar(precip_pd.index, precip_pd[0], width=precip_pd['width'], color='#377EB8', linewidth=0)
-plt.ylabel('mmWE / year')
-plt.ylim(0, 200)
-plt.yticks([0, 40, 80, 120, 160], [0, 40, 80, 120, 160])
-plt.tick_params(axis='x', bottom='off', top='off')
+	ax_rad.spines['left'].axis.axes.tick_params(direction='outward')
+	ax_rad.yaxis.tick_left()
+	#ax_rad.xaxis.tick_bottom()
+	plt.tick_params(axis='x', bottom='off', top='off')
+	ax_rad.spines['top'].set_visible(False)
+	ax_rad.spines['right'].set_visible(False)
+	ax_rad.spines['bottom'].set_visible(False)
+	ax_rad.annotate('a', fontsize=8, fontweight='bold', xy=(0.05,0.95), xycoords='axes fraction',
+	           horizontalalignment='left', verticalalignment='top')
 
-# JJA total precip
-ax_shf = plt.twinx()
-SHF_JJA.plot(label='SHF', marker='o', color='black', markersize=4, mec='none')
-plt.ylabel('W m$^{-2}$')
 
-ax_precip.spines['left'].axis.axes.tick_params(direction='outward')
-ax_precip.yaxis.tick_left()
-#ax_precip.xaxis.tick_bottom()
-ax_precip.spines['top'].set_visible(False)
-ax_precip.spines['right'].set_visible(False)
-ax_precip.spines['bottom'].set_visible(False)
-ax_precip.annotate('b', fontsize=8, fontweight='bold', xy=(0.05,0.95), xycoords='axes fraction',
-           horizontalalignment='left', verticalalignment='top')
 
-ax_shf.spines['right'].axis.axes.tick_params(direction='outward')
-ax_shf.yaxis.tick_right()
-#ax_shf.xaxis.tick_bottom()
-plt.tick_params(axis='x', bottom='off', top='off')
-ax_shf.spines['top'].set_visible(False)
-ax_shf.spines['left'].set_visible(False)
-ax_shf.spines['bottom'].set_visible(False)
+	## Subplot: other fluxes -----------------------------------------------------
+	ax_precip = plt.subplot(3, 1, 2, sharex=ax_rad)
+	precip_pd = RF_avg.to_pandas().to_frame()
+	precip_pd = precip_pd.assign(width=(365 - 100))
+	# Deal with leap years
+	precip_pd.loc[precip_pd.index.is_leap_year, 'width'] = 366 - 100
+	plt.bar(precip_pd.index, precip_pd[0], width=precip_pd['width'], color='#377EB8', linewidth=0)
+	plt.ylabel('mmWE / year')
+	plt.ylim(0, 200)
+	plt.yticks([0, 40, 80, 120, 160], [0, 40, 80, 120, 160])
+	plt.tick_params(axis='x', bottom='off', top='off')
 
-## Subplot: ratios (of absolute fluxes, not anomalies) -----------------------
+	# JJA total precip
+	ax_shf = plt.twinx()
+	SHF_JJA.plot(label='SHF', marker='o', color='black', markersize=4, mec='none')
+	plt.ylabel('W m$^{-2}$')
 
-ax_ratios = plt.subplot(3, 1, 3, sharex=ax_rad)
-(LWD_JJA / SWD_JJA).plot(label='LWD : SWD', marker='o', color='black', markersize=4, mec='none')
-((LWD_JJA + SHF_JJA) / SWD_JJA).plot(label='LWD+SHF : SWD', marker='o', color='black', linestyle=(0, (2,1)), markersize=4, mec='none')
+	ax_precip.spines['left'].axis.axes.tick_params(direction='outward')
+	ax_precip.yaxis.tick_left()
+	#ax_precip.xaxis.tick_bottom()
+	ax_precip.spines['top'].set_visible(False)
+	ax_precip.spines['right'].set_visible(False)
+	ax_precip.spines['bottom'].set_visible(False)
+	ax_precip.annotate('b', fontsize=8, fontweight='bold', xy=(0.05,0.95), xycoords='axes fraction',
+	           horizontalalignment='left', verticalalignment='top')
 
-plt.ylabel('Ratio')
+	ax_shf.spines['right'].axis.axes.tick_params(direction='outward')
+	ax_shf.yaxis.tick_right()
+	#ax_shf.xaxis.tick_bottom()
+	plt.tick_params(axis='x', bottom='off', top='off')
+	ax_shf.spines['top'].set_visible(False)
+	ax_shf.spines['left'].set_visible(False)
+	ax_shf.spines['bottom'].set_visible(False)
 
-plt.xlabel('Year')
+	## Subplot: ratios (of absolute fluxes, not anomalies) -----------------------
 
-ax_ratios.spines['left'].axis.axes.tick_params(direction='outward')
-ax_ratios.yaxis.tick_left()
-ax_ratios.xaxis.tick_bottom()
-ax_ratios.spines['top'].set_visible(False)
-ax_ratios.spines['right'].set_visible(False)
-ax_ratios.spines['bottom'].set_position(('outward', 10))
-ax_ratios.annotate('c', fontsize=8, fontweight='bold', xy=(0.05,0.95), xycoords='axes fraction',
-           horizontalalignment='left', verticalalignment='top')
+	ax_ratios = plt.subplot(3, 1, 3, sharex=ax_rad)
+	(LWD_JJA / SWD_JJA).plot(label='LWD : SWD', marker='o', color='black', markersize=4, mec='none')
+	((LWD_JJA + SHF_JJA) / SWD_JJA).plot(label='LWD+SHF : SWD', marker='o', color='black', linestyle=(0, (2,1)), markersize=4, mec='none')
 
-plt.tight_layout()
-plt.savefig('/home/at15963/Dropbox/work/papers/tedstone_darkice/submission1/figures/radiative_fluxes.pdf')
+	plt.ylabel('Ratio')
+
+	plt.xlabel('Year')
+
+	ax_ratios.spines['left'].axis.axes.tick_params(direction='outward')
+	ax_ratios.yaxis.tick_left()
+	ax_ratios.xaxis.tick_bottom()
+	ax_ratios.spines['top'].set_visible(False)
+	ax_ratios.spines['right'].set_visible(False)
+	ax_ratios.spines['bottom'].set_position(('outward', 10))
+	ax_ratios.annotate('c', fontsize=8, fontweight='bold', xy=(0.05,0.95), xycoords='axes fraction',
+	           horizontalalignment='left', verticalalignment='top')
+
+	plt.tight_layout()
+	plt.savefig('/home/at15963/Dropbox/work/papers/tedstone_darkice/submission1/figures/radiative_fluxes.pdf')
