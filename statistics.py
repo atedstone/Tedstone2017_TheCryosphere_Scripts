8c27b698c268402287caa2e2114cfd5ffc9a5777
Andrew Tedstone <a.j.tedstone@bristol.ac.uk>
2017-03-06 15:52:29 +0000

    various tweaks to statistics.py

--- statistics.py
+++ statistics.py
@@ -16,6 +16,10 @@ toplot = as_perc \
 	.where(onset.dark_dur > 5) \
 	.where(mask_dark == 1)
 
+# B01 average post-bare
+B01_avg_bare = b01_avg.where(modis_periods_bare2end) \
+	.resample('1AS', dim='TIME', how='mean').to_pandas()
+
 
 # Melt anomaly
 ME_all_long = mar_raster.open_mfxr(mar_path_anomalies,
@@ -56,7 +60,9 @@ ttmin_JJA_count_pd = ttmin_JJA_count.to_pandas().squeeze()
 ttmin_JJA_count_pd.index = pd.date_range('2000-01-01', '2016-01-01', freq='1AS')
 
 
-
+dark_perc = toplot.median(dim=('X', 'Y')).to_pandas()
+B01_avg = b01_avg.where(b01_avg['TIME.season'] == 'JJA').resample('1AS', dim='TIME', how='mean').to_pandas()
+dark_norm = dark_perc / (B01_avg * 100)
 
 ### Construct data frames ----------------------------------------------------
 
@@ -64,10 +70,12 @@ ttmin_JJA_count_pd.index = pd.date_range('2000-01-01', '2016-01-01', freq='1AS')
 df_jja = pd.DataFrame({
 		'SWD_anom': SWD_JJA_anom.to_pandas(),
 		'LWD_anom': LWD_JJA_anom.to_pandas(),
-		'SHF_anom': SHF_JJA.to_pandas(),
+		'SHF_anom': SHF_JJA_anom.to_pandas(),
 		'RF_avg': RF_avg.to_pandas(),
-		'dark_perc': toplot.median(dim=('X', 'Y')).to_pandas(),
-		'B01_avg': b01_avg.where(b01_avg['TIME.season'] == 'JJA').resample('1AS', dim='TIME', how='mean').to_pandas(),
+		'dark_perc': dark_perc,
+		'B01_avg': B01_avg,
+		'B01_avg_bare': B01_avg_bare,
+		'dark_norm': dark_norm,
 		'melt': ME_JJA_anom.to_pandas().squeeze(),
 		'snow_clear_doy': onset.bare.where(mask_dark > 0).mean(dim=['X','Y']).to_pandas(),
 		'ttmin_count': ttmin_JJA_count_pd,
@@ -76,6 +84,7 @@ df_jja = pd.DataFrame({
 
 print(df_jja.corr(method='spearman').round(2))
 
+df_jja.to_excel('/home/at15963/Dropbox/work/papers/tedstone_darkice/submission1/statistics_df_jja.xls')
 ## Implement tests for normality...but do they actually tell us anything? (see belog post...)
 
 
@@ -84,14 +93,16 @@ print(df_jja.corr(method='spearman').round(2))
 ### Regression for JJA annual values -----------------------------------------
 
 X_vars = ('SHF_anom', 'melt', 'snow_clear_doy', 'ttmin_count', 'LWD_SHF_vs_SWD')
+Y_vars = ('B01_avg', 'B01_avg_bare', 'dark_perc', 'dark_norm')
 
-for X_var in X_vars:
-	X = df_jja[X_var]
-	y = df_jja.dark_perc #or B01_avg
-	X = sm.add_constant(X)
-	model = sm.OLS(y, X) # or QuantReg
-	results = model.fit() # if QuantReg, can pass p=percentile here
-	print(results.summary())
+for Y_var in Y_vars:
+	for X_var in X_vars:
+		X = df_jja[X_var]
+		y = df_jja[Y_var]
+		X = sm.add_constant(X)
+		model = sm.OLS(y, X) # or QuantReg
+		results = model.fit() # if QuantReg, can pass p=percentile here
+		print(results.summary())
 
 
 
@@ -99,11 +110,11 @@ for X_var in X_vars:
 
 plt.figure()
 n = 1
-for y_var in ('B01_avg', 'dark_perc'):
+for y_var in Y_vars:
 	for x_var in X_vars:
-		ax = subplot(2, 5, n)
+		ax = subplot(4, 5, n)
 		plt.plot(df_jja[x_var], df_jja[y_var], 'o', mfc='#377EB8', mec='none', alpha=0.8)
-		if y_var == 'dark_perc':
+		if y_var == 'dark_norm':
 			plt.xlabel(x_var)
 		
 		if x_var == 'SHF_anom':
