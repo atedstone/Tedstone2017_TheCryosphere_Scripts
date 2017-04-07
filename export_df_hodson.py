15dd68e628247614500281d97604000b3a0b0165
Andrew Tedstone <a.j.tedstone@bristol.ac.uk>
2017-04-07 17:14:49 +0100

    add code to export cumulative dark ice extent for andy

--- export_df_hodson.py
+++ export_df_hodson.py
@@ -20,6 +20,7 @@ b01_avg = read_data(store_path + 'B01_avg_clouds50_daily.csv', 'B01_avg')
 shf_abs = read_data(store_path + 'SHF_abs.csv', 'SHF')
 sw_abs = read_data(store_path + 'SW_abs.csv', 'SW')
 lw_abs = read_data(store_path + 'LW_abs.csv', 'LW')
+darkice = read_data(store_path + 'cumulative_darkice.csv', 'darkice_cumsum')
 
-df = pd.concat((b01_avg, shf_abs, sw_abs, lw_abs, al), axis=1)
+df = pd.concat((b01_avg, shf_abs, sw_abs, lw_abs, al, darkice), axis=1)
 df.to_csv(store_path + 'b01_shf_lw_sw.csv')
--- plot_annual_cumulative_common_dark.py
+++ plot_annual_cumulative_common_dark.py
@@ -13,6 +13,13 @@ rcParams['font.size'] = 6
 fig = plt.figure(figsize=(5.5, 3))
 
 
+## Copied from analyse_snow.py
+SHSN2_all = mar_raster.open_mfxr(mar_path,
+	dim='TIME', transform_func=lambda ds: ds.SHSN2.sel(X=x_slice, 
+		Y=y_slice))
+snow_above_ice = SHSN2_all.sel(SECTOR1_1=1.0).where(mar_mask_dark.r > 0).where((SHSN2_all['TIME.month'] >= 4) & (SHSN2_all['TIME.month'] < 9)).mean(dim=('X', 'Y')).to_pandas()
+
+
 # Plot amount of snow above ice on the same axes as above
 # (requires data import from analyze_snow.py)
 from matplotlib import dates
@@ -33,7 +40,7 @@ for year, data in snow_above_ice.groupby(snow_above_ice.index.year):
 
 	if n in (1, 6, 11, 16):
 		plt.yticks([0, 0.5, 1, 1.5], [0, 0.5, 1, 1.5])
-		ax.spines['left'].axis.axes.tick_params(direction='outward')
+		ax.tick_params(axis='y', direction='out')
 
 	else:
 		plt.yticks([])
@@ -51,10 +58,11 @@ for year, data in snow_above_ice.groupby(snow_above_ice.index.year):
 
 	n += 1
 
+csum_store = []
 n = 1
 for year in onset.TIME:
 
-	ax = subplot(4, 5, n)
+	ax = plt.subplot(4, 5, n)
 	ax2 = ax.twinx()
 	y = pd.to_datetime(year.values).strftime('%Y')
 
@@ -63,7 +71,11 @@ for year in onset.TIME:
 
 	hist = hist * 0.377
 	bin_dates = [dt.datetime.strptime('%s %s' %(y, int(b)), '%Y %j') for b in bin_edges]
-	plt.plot(bin_dates[:-1], np.cumsum(hist), '-', color='#CB181D', lw=2, alpha=0.8)
+	csum = np.cumsum(hist)
+	plt.plot(bin_dates[:-1], csum, '-', color='#CB181D', lw=2, alpha=0.8)
+	csum_pd = pd.Series(csum, index=bin_dates[:-1])
+	csum_pd.to_csv(store_path + 'darkice_cumulative_%s.csv' %y)
+	csum_store.append(csum_pd)
 
 	# change this to only be plotted when there are 'sufficient' good quality obs.
 	#plt.plot(bin_edges[:-1], np.cumsum(hist), 'o', mec='#08306B', mfc='none', markersize=4, alpha=0.4)
@@ -87,7 +99,7 @@ for year in onset.TIME:
 	ax2.yaxis.tick_right()
 	if n in (5, 10, 15, 17):
 		plt.yticks([0, 40000, 80000, 120000], [0, 4, 8, 12])
-		ax2.spines['right'].axis.axes.tick_params(direction='outward')
+		ax2.tick_params(axis='y', direction='out')
 	else:
 		plt.yticks([])
 		ax2.spines['right'].set_visible(False)
@@ -101,8 +113,8 @@ for year in onset.TIME:
 		ax2.spines['bottom'].set_visible(False)
 		plt.xticks(ax.xaxis.get_ticklocs(), [])
 	else:
-		ax2.spines['bottom'].axis.axes.tick_params(direction='outward')
-		ax.spines['bottom'].axis.axes.tick_params(direction='outward')
+		ax2.tick_params(direction='out')
+		ax.tick_params(direction='out')
 		plt.tick_params(axis='x', bottom='on', top='off')
 
 	if n == 17:
@@ -122,7 +134,8 @@ fig.text(0.51, 0.03, 'Month of Year', ha='center', va='center', color='black')
 
 plt.savefig('/home/at15963/Dropbox/work/papers/tedstone_darkice/submission1/figures/snowdepth_darkice.pdf')
 
-
+csum_all = pd.concat(csum_store)
+csum_all.to_csv(store_path + 'cumulative_darkice.csv')
 
 
 
