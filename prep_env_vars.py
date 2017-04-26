7714d4e985379630d9f0f760f5806eb86754a9a4
Andrew Tedstone <a.j.tedstone@bristol.ac.uk>
2017-04-26 18:40:35 +0100

    improve onset windowing strategy

--- prep_env_vars.py
+++ prep_env_vars.py
@@ -57,7 +57,7 @@ sns.set_style("white")
 ds_refl = xr.open_mfdataset('/scratch/MOD09GA.006.SW/*b1234q.nc',
 		chunks={'TIME':366})
 
-onset = xr.open_dataset('/scratch/physical_controls/MOD09GA.006.2000-2016.dark45.postsnow.JJA.nc')
+onset = xr.open_dataset('/scratch/physical_controls/MOD09GA.006.onset.2000-2016.bare60.dark45.JJA.win7.b3.d3.nc')
 
 mar_path = '/scratch/MARv3.6.2-7.5km-v2-ERA/ICE.20*nc'
 x_slice = slice(-586678,-254545)
