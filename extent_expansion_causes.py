6db10eef221e5e986c5c08fd9b52b6a12adeb94f
Andrew Tedstone <a.j.tedstone@bristol.ac.uk>
2017-04-28 17:11:59 +0100

    analysis outputs for manuscript added

--- extent_expansion_causes.py
+++ extent_expansion_causes.py
@@ -1,8 +1,16 @@
+from prep_env_vars import *
+import pandas as pd
+
 df = pd.read_csv(store_path + 'timeseries_df.csv', index_col=0, parse_dates=True)
+
+
+### Add data columns 
 
 # Add TTMIN column
 ttmin = pd.read_csv(store_path + 'TTMIN_daily.csv', index_col=0, skiprows=1, header=None, parse_dates=True).squeeze()
 df = df.assign(ttmin=ttmin)
+
+# Remove non-JJA data
 df = df[(df.index.month >= 6) & (df.index.month < 9)]
 
 # Compute n.days of continuous TTMIN > 0
@@ -46,6 +54,41 @@ df = df.assign(clouds_cont=r)
 # Compute daily dark ice extent increase and add to df
 df = df.assign(darkice_increase=df.darkice_cumsum.diff())
 
+# Remove days when no dark ice has appeared yet
+df = df[df.darkice_cumsum > 0]
+
+## Categorise dark ice increase days
+# common area size in km
+ca = 10409
+df = df.assign(big=np.where(((100 / ca) * df.darkice_increase >= 5) & (df.darkice_cumsum < ca-1040), 1, 0))
+df = df.assign(massive=np.where(((100 / ca) * df.darkice_increase >= 10) & (df.darkice_cumsum < ca-1040), 1, 0))
+
+df = df.assign(cloud_7d_mean=df.cloud_perc.rolling(7).mean())
+df = df.assign(cloud_7d_std=df.cloud_perc.rolling(7).std())
+
+### Analysis, Part A - check that big expansion events aren't an artefact of 
+### several days preceding cloud cover
+
+
+print('N.obs .... big: %s, rest: %s' % (df[df.big == 1].count(), df[df.big == 0].count()))
+print('N.days preceding cloud cover > 50 perc .... big: %s, rest: %s' % (df[df.big == 1].clouds_cont.mean(), df[df.big == 0].clouds_cont.mean()))
+print('TTMIN_count .... big: %s, rest: %s' % (df[df.big == 1].ttmin_cont.median(), df[df.big == 0].ttmin_cont.median()))
+print('SHF .... big: %s, rest: %s' % (df[df.big == 1].SHF.mean(), df[df.big == 0].SHF.mean()))
+print('SHFstd .... big: %s, rest: %s' % (df[df.big == 1].SHF.std(), df[df.big == 0].SHF.std()))
+
+print('cloud_7d_mean .... big: %s, rest: %s' % (df[df.big == 1].cloud_7d_mean.mean(), df[df.big == 0].cloud_7d_mean.mean()))
+print('cloud_7d_std .... big: %s, rest: %s' % (df[df.big == 1].cloud_7d_std.mean(), df[df.big == 0].cloud_7d_std.mean()))
+
+
+# Percentage increase in absolute SHF on dark expanison days
+(100 / 29) * 57
+
+
+
+
+### Stuff below this line not currently in use ----------------------------
+
+
 # Compute SWnet
 df = df.assign(sw_net=df.SW * (1 - (df.al / 100)))
 
@@ -79,4 +122,8 @@ df_weekly = df_weekly.assign(daily_contrib=(100 / df_weekly.darkice_increase) * 
 df_weekly = df_weekly.assign(darkice_today=df.darkice_increase)
 
 
-# Add e.g. 'big' indicator column to be able to use seaborn facet options
+from scipy import stats
+stat, pval = stats.ks_2samp(df_big.ttmin_cont, df_other.ttmin_cont)
+
+stats.mannwhitneyu(df_big.ttmin_cont, df_other.ttmin_cont)
+stats.ranksums(df_big.ttmin_cont, df_other.ttmin_cont)
